6aeac5e1900e6a18ec959e107bad681f46c8028e
Prasanna V <you@example.com>
2024-02-27 22:43:25 +0530

    coded

--- src/components/LikeImage.jsx
+++ src/components/LikeImage.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function LikeImage() {
-
   const [likeImageCounter, setLikeImageCounter] = useState(0);
 
-  const handleLikeImageCount = ()=>{
-    setLikeImageCounter(likeImageCounter+1);
-  }
+  const handleLikeImageCount = () => {
+    setLikeImageCounter(likeImageCounter + 1);
+    console.log(likeImageCounter,"like");
+  };
 
   return (
     <div>
-      <button onClick={handleLikeImageCount}>Like Image {likeImageCounter}</button>
+      <button onClick={handleLikeImageCount}>
+        Like Image {likeImageCounter}
+      </button>
     </div>
-  )
+  );
 }
